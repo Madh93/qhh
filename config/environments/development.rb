9f650e5d60c197b28df18c80f9ea679f63126c6b
Miguel Hernandez <michelangelo93@gmail.com>
2015-01-09 19:13:32 +0000

    Editado localhost de email por el del servidor

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ Qhh::Application.configure do
   
   # Mailer
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'localhost:8080' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
