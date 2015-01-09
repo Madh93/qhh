576b3b365594987b5718cfdab8de52d1cf0fc394
Madh93 <michelangelo93@gmail.com>
2015-01-09 18:32:43 +0000

    Añadido scroll to top

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,18 @@
 //= require bootstrap 
 //= require common
 
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
