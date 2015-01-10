8d1deaa5d46bedb035d468bb5d31ed04229d5e71
Madh93 <michelangelo93@gmail.com>
2015-01-10 17:22:39 +0000

    Terminado de foramlziar categorias

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,31 +2,35 @@
 # Initial seed file to use with Devise User Model
 
 # Temporary admin account
-u = User.new(
-    username: "admin",
-    email: "admin@example.com",
-    password: "1234",
-    password_confirmation: "1234",
-    admin: true
-)
-u.skip_confirmation!
-u.save!
+# u = User.new(
+#     username: "admin",
+#     email: "admin@example.com",
+#     password: "1234",
+#     password_confirmation: "1234",
+#     admin: true
+# )
+# u.skip_confirmation!
+# u.save!
 
 
 
-# Test user accounts
-(1..50).each do |i|
-  u = User.new(
-      username: "user#{i}",
-      email: "user#{i}@example.com",
-      password: "1234",
-      password_confirmation: "1234"
-  )
-  u.skip_confirmation!
-  u.save!
+# # Test user accounts
+# (1..50).each do |i|
+#   u = User.new(
+#       username: "user#{i}",
+#       email: "user#{i}@example.com",
+#       password: "1234",
+#       password_confirmation: "1234"
+#   )
+#   u.skip_confirmation!
+#   u.save!
 
-  puts "#{i} test users created..." if (i % 5 == 0)
+#   puts "#{i} test users created..." if (i % 5 == 0)
 
-end
+# end
   
-
+# categorias = ["Aire libre", "Café", "Cine", "Comida", "Compras", "Deporte", "Entretenimiento", "Noche"]
+# categorias.each do |cat|
+#   a = Category.new(name: cat)
+#   a.save!
+# end
