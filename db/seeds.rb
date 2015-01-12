2ab105138c15a720d2f438b2b8a6886b67a568d4
Madh93 <michelangelo93@gmail.com>
2015-01-12 17:25:51 +0000

    Estructura basica para el seed de sitios

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,3 +34,25 @@
 #   a = Category.new(name: cat)
 #   a.save!
 # end
+
+
+
+##SITIOS SEED
+# sitios.each do |sit|
+#     s = Site.new(
+#         name: sit[0],       #string
+#         category: sit[1],   #integer
+#         tags: sit[2],       #string
+#         address: sit[3],    #string
+#         postal: sit[4],     #integer
+#         phone: sit[5],      #string
+#         mail: sit[6],       #string
+#         webpage: sit[7],    #string
+#         latitude: sit[8],   #float x.123456
+#         longitude: sit[9],  #float
+#         text1: sit[10],     #text
+#         text2: sit[11],     #text
+#         text3: sit[12]      #text
+#     )
+#     s.save!
+# end
