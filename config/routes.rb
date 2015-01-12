b1cde67e171106a6d636e8ffcaed6cefcb59104f
Madh93 <michelangelo93@gmail.com>
2015-01-12 15:01:22 +0000

    Quitar mensajes de error

--- config/routes.rb
+++ config/routes.rb
@@ -17,9 +17,14 @@ Qhh::Application.routes.draw do
     resources :posts
   end
 
+  # Nuevas paginas
   get "/myqhh", to: "pages#myqhh", as: "myqhh"
   get "/encuentra", to: "pages#encuentra", as: "encuentra"
   get "/categorias/:id", to: "pages#show_category", as: "categoria"
 
-  
+  # Mensajes de error
+  match '/404', to: 'errors#file_not_found', via: :all
+  match '/422', to: 'errors#unprocessable', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
+
 end
