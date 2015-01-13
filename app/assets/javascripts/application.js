c86b39cbbf9e4555616bc951defc6bdf9d2bf319
Madh93 <michelangelo93@gmail.com>
2015-01-13 17:59:01 +0000

    Creado JS y SCSS para migas de pan

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,18 +16,29 @@
 //= require bootstrap 
 //= require common
 
+// SCROLL UP
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $('.scrollup').fadeIn();
+    } else {
+        $('.scrollup').fadeOut();
+    }
+});
 
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.scrollup').fadeIn();
-        } else {
-            $('.scrollup').fadeOut();
-        }
-    });
+$('.scrollup').click(function () {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 600);
+    return false;
+});
 
-    $('.scrollup').click(function () {
-        $("html, body").animate({
-            scrollTop: 0
-        }, 600);
-        return false;
-    });
+//BREADCRUMBS
+$(window).resize(function() {
+
+    ellipses1 = $("#bc1 :nth-child(2)")
+    if ($("#bc1 a:hidden").length >0) {ellipses1.show()} else {ellipses1.hide()}
+    
+    ellipses2 = $("#bc2 :nth-child(2)")
+    if ($("#bc2 a:hidden").length >0) {ellipses2.show()} else {ellipses2.hide()}
+    
+})
