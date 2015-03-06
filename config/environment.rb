586e1ed1b3ba69202d9e81640d5bf56d927329bd
Madh93 <michelangelo93@gmail.com>
2015-03-06 18:56:24 +0000

    ajustando mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Qhh::Application.initialize!
+
+
+ActionMailer::Base.smtp_settings = {
+    address: ENV["SMTP_SERVER"],
+    openssl_verify_mode: OpenSSL::SSL::VERIFY_NONE,
+    port: ENV["SMTP_PORT"].to_i,
+    domain: ENV["MAILER_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["SMTP_USER"],
+    password: ENV["SMTP_PWD"]
+}
